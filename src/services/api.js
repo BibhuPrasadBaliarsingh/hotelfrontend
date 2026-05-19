72a48d3bb06715481919dfab80eaf0e351d49f56
BibhuPrasadBaliarsingh <bibhuprasadbaliarsingh@gmail.com>
2026-05-19 16:37:53 +0530

    update changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // baseURL:"https://hotelbackend-rsrl.onrender.com/api",
-  baseURL:"http://localhost:5000/api/",
+  baseURL:"https://hotelbackend-rsrl.onrender.com/api",
+  // baseURL:"http://localhost:5000/api/",
   timeout: 10000,
 });
 
